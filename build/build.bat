e77fabb7d88d92d1a9c70b0c4714adc9073ff316
weetao <hedeath@gmail.com>
2025-01-01 17:39:50 +0800

    create output dir if it not exist

--- build/build.bat
+++ build/build.bat
@@ -8,6 +8,7 @@ for /f %%i in ('dir /B /A:D ..\maps\*') do (
 	xcopy /Q /E /Y ..\mission .\%%i\
 	xcopy /Q /E /Y ..\maps\%%i .\%%i\
 	if exist .\custom\ xcopy /Q /E /Y .\custom .\%%i\
+	if not exist ..\build_output\ mkdir ..\build_output\
 	bin\PBOConsole.exe -pack %%i ..\build_output\vn_mf.%%i.pbo  > nul 2>&1
 	rmdir /S /Q %%i
 	echo Done.
